f5dccacb8f88d2fb7dfef26c3de1128ca9c36a80
tharinm <tharindumadhushan864@gmail.com>
2023-05-12 17:42:12 +0530

    developed backend for update book

--- backend/controller/books.controller.js
+++ backend/controller/books.controller.js
@@ -1,5 +1,6 @@
 import { db } from "../server.js";
 
+//get all books
 export const getBooks = async (req, res, next) => {
   const q = "SELECT * FROM books;";
   if (db.state === "disconnected") {
@@ -15,6 +16,7 @@ export const getBooks = async (req, res, next) => {
   });
 };
 
+//create new Book
 export const createBook = async (req, res, next) => {
   const q =
     "INSERT INTO books (`title`,`description`,`price`,`cover`) VALUES (?);";
@@ -26,17 +28,18 @@ export const createBook = async (req, res, next) => {
     req.body.cover,
   ];
 
-  console.log(req.body.cover)
+  console.log(req.body.cover);
 
   db.query(q, [values], (err, data) => {
     if (err) {
-      console.log(err)
+      console.log(err);
       return res.json(err);
     }
     return res.json("Book has been created successfully");
   });
 };
 
+//delete book
 export const deleteBook = async (req, res, next) => {
   //request from url
   const getId = req.params.id;
@@ -48,5 +51,27 @@ export const deleteBook = async (req, res, next) => {
       return res.json(err);
     }
     return res.json("Book has been deleted successfully");
+  });
+};
+
+//update book
+export const updateBook = async (req, res, next) => {
+  const getId = req.params.id;
+
+  const q = `UPDATE books SET title = ?, description = ?, price = ?, cover = ? WHERE id = ?;`;
+
+
+  const values = [
+    req.body.title,
+    req.body.description,
+    req.body.price,
+    req.body.cover,
+  ];
+
+  db.query(q, [...values,getId], (err, data) => {
+    if (err) {
+      return res.json(err);
+    }
+    return res.json("Book has been updated successfully");
   });
 };
--- backend/routes/books.routes.js
+++ backend/routes/books.routes.js
@@ -3,6 +3,7 @@ import {
   getBooks,
   createBook,
   deleteBook,
+  updateBook,
 } from "../controller/books.controller.js";
 
 const route = express.Router();
@@ -10,5 +11,6 @@ const route = express.Router();
 route.get("/books", getBooks);
 route.post("/books", createBook);
 route.delete("/books/:id", deleteBook);
+route.put("/books/:id", updateBook);
 
 export default route;
